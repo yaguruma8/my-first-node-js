b01704ad19fcf76a724add9afa1015f05be746fd
yaguruma <yaguruma_y@yahoo.co.jp>
2021-03-13 18:50:48 +0900

    階乗関数wo

---
+++ factorial.js
@@ -0,0 +1,36 @@
+'use strict';
+/**
+ * 与えられた自然数の階乗を返す
+ * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+ * @param {Number} n
+ * @returns {Number}
+ */
+function factorial(n) {
+    let result = 1;
+    for (let i = n; i > 0; i--) {
+        result *= i;
+    }
+    return result;
+}
+const assert = require('assert');
+assert.strictEqual(
+    factorial(1),
+    1,
+    `1の階乗は1ですが、実際は${factorial(1)}でした`
+);
+assert.strictEqual(
+    factorial(2),
+    2,
+    `2の階乗は2ですが、実際は${factorial(2)}でした`
+);
+assert.strictEqual(
+    factorial(3),
+    6,
+    `3の階乗は6ですが、実際は${factorial(3)}でした`
+);
+assert.strictEqual(
+    factorial(10),
+    3628800,
+    `10の階乗は3628800ですが、実際は${factorial(10)}でした`
+);
+console.log('すべてのテストを通過しました');
